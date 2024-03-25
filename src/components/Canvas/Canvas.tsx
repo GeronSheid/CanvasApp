c5431acba711b414f9283b40fae991527faa76b4
Dmitry Shneider <dmitryshneider@gmail.com>
2024-03-26 00:46:53 +0600

    added SLOWDOWN_PARAM const

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -16,47 +16,49 @@ export interface IProps {
 
 type Props = React.DetailedHTMLProps<React.CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement>
     &
-IProps;
+    IProps;
 
 
 
 
-const Canvas: React.FC<Props> = ({balls, ...props }) => {
+const Canvas: React.FC<Props> = ({ balls, ...props }) => {
+    const SLOWDOWN_PARAM = -0.7;
+
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
     const mouseDown = useRef<boolean>(false);
-    const mousePrevPos = useRef<{x: number, y: number}>({x: 0, y: 0})
-    const mousePos = useRef<{x: number, y: number}>({x: 0, y: 0})
+    const mousePrevPos = useRef<{ x: number, y: number }>({ x: 0, y: 0 })
+    const mousePos = useRef<{ x: number, y: number }>({ x: 0, y: 0 })
 
-    const isInside = (mx: number, my: number, ball: IBall) : boolean => {
+    const isInside = (mx: number, my: number, ball: IBall): boolean => {
         const distX = mx - ball.x;
         const distY = my - ball.y;
         const distance = Math.sqrt(distX ** 2 + distY ** 2);
-        return distance <= ball.r
+        return distance <= ball.r;
     }
 
     const handleMouseDown = (e: MouseEvent) => {
         const rectangle = canvasRef.current?.getBoundingClientRect() as DOMRect;
         const mouse_x = e.clientX - rectangle.left;
         const mouse_y = e.clientY - rectangle.top;
-        
-        if(e.buttons === 1) {
+
+        if (e.buttons === 1) {
             mouseDown.current = true;
-        } 
+        }
         balls.forEach(ball => {
-            
-            if(isInside(mouse_x, mouse_y, ball) && e.buttons === 1) {
+
+            if (isInside(mouse_x, mouse_y, ball) && e.buttons === 1) {
                 props.setBallIndex(balls.indexOf(ball));
                 props.setModalVisible(true);
-                mouseDown.current = false
+                mouseDown.current = false;
             }
         })
     }
 
     const handleMouseUp = (e: MouseEvent) => {
-        
-            mouseDown.current = false;
-        
+
+        mouseDown.current = false;
+
     }
 
 
@@ -64,23 +66,23 @@ const Canvas: React.FC<Props> = ({balls, ...props }) => {
         const rectangle = canvasRef.current?.getBoundingClientRect() as DOMRect;
         const mouse_x = e.clientX - rectangle.left;
         const mouse_y = e.clientY - rectangle.top;
-        mousePrevPos.current = {x: mousePos.current.x, y: mousePos.current.y }
-        mousePos.current = {x: mouse_x, y: mouse_y}
+        mousePrevPos.current = { x: mousePos.current.x, y: mousePos.current.y };
+        mousePos.current = { x: mouse_x, y: mouse_y };
 
         const dmx = mousePos.current.x - mousePrevPos.current.x;
         const dmy = mousePos.current.y - mousePrevPos.current.y;
 
         balls.forEach(ball => {
-            if(isInside(mouse_x, mouse_y, ball) && mouseDown.current) {
+            if (isInside(mouse_x, mouse_y, ball) && mouseDown.current) {
                 ball.dx = dmx;
                 ball.dy = dmy;
             }
         })
-        
+
     }
 
     useEffect(() => {
-        
+
         canvasRef.current?.addEventListener('mousemove', handleMouseMove)
         canvasRef.current?.addEventListener('mousedown', handleMouseDown)
         canvasRef.current?.addEventListener('mouseup', handleMouseUp)
@@ -117,23 +119,23 @@ const Canvas: React.FC<Props> = ({balls, ...props }) => {
                 ball.x += ball.dx;
                 ball.y += ball.dy;
 
-                if(ball.x - ball.r < 0) {
+                if (ball.x - ball.r < 0) {
                     ball.x = ball.r;
-                    ball.dx *= -0.9;
-                } else if(ball.x + ball.r > canvas.width) {
+                    ball.dx *= SLOWDOWN_PARAM;
+                } else if (ball.x + ball.r > canvas.width) {
                     ball.x = canvas.width - ball.r;
-                    ball.dx *= -0.9;
+                    ball.dx *= SLOWDOWN_PARAM;
                 }
 
-                if(ball.y - ball.r < 0) {
+                if (ball.y - ball.r < 0) {
                     ball.y = ball.r;
-                    ball.dy *= -0.9;
-                } else if(ball.y + ball.r > canvas.height) {
+                    ball.dy *= SLOWDOWN_PARAM;
+                } else if (ball.y + ball.r > canvas.height) {
                     ball.y = canvas.height - ball.r;
-                    ball.dy *= -0.9;
+                    ball.dy *= SLOWDOWN_PARAM;
                 }
 
-                
+
 
 
 
@@ -147,12 +149,12 @@ const Canvas: React.FC<Props> = ({balls, ...props }) => {
                         const overlap_x = Math.cos(angle) * overlap;
                         const overlap_y = Math.sin(angle) * overlap;
                         if (distance < ball.r + otherBall.r) {
-                            otherBall.dx = (otherBall.dx + ball.dx) * -0.7;
-                            otherBall.dy = (otherBall.dy + ball.dy) * -0.7;
+                            otherBall.dx = (otherBall.dx + ball.dx) * SLOWDOWN_PARAM;
+                            otherBall.dy = (otherBall.dy + ball.dy) * SLOWDOWN_PARAM;
                             otherBall.x -= overlap_x / 2;
-                            otherBall.y -= overlap_y /2;
+                            otherBall.y -= overlap_y / 2;
                         }
-                        
+
                     }
                 })
 
